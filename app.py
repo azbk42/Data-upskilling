49d920b6a0b1b6ad21ef4074f7f725ac009e4953
Elouan Mauduit <emauduit@bess-f4r1s2.clusters.42paris.fr>
2025-01-07 15:08:49 +0100

    ajout interpreteur sql avec duckdb + streamlit

--- app.py
+++ app.py
@@ -1,3 +1,25 @@
 import streamlit as st
+import pandas as pd
+import duckdb
 
-st.write("Salut a tous")
+data = {'a': [1, 2, 3], 'b': [4, 5, 6]}
+df = pd.DataFrame(data)
+
+st.write("Hello World")
+
+tab1, tab2, tab3 = st.tabs(['cat', 'chien', 'oiseau'])
+
+with tab1:
+    input_t = st.text_area(label="put ton texte:")
+    if input_t:
+        result = duckdb.query(input_t).df()
+        st.write(input_t)
+        st.dataframe(result)
+
+with tab2:
+    input_te = st.text_area(label="pfewfwete:")
+    st.write(input_te)
+
+with tab3:
+    input_tf = st.text_area(label="put tofewfwefwen texte:")
+    st.write(input_tf)
